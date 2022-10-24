32bad16dc43021acc68d2caacbee2962d6f42c6c
moksha <dullstart.yang@gmail.com>
2022-10-24 13:29:44 +0800

    表單練習

---
+++ bmi.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI計算</title>
+</head>
+<body>
+    <a href="index.php">回首頁</a>
+    <br>
+    <h1>BMI計算</h1>
+</body>
+</html>
