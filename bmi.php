11e274401719329ff7581c70226751316ccc1e0b
moksha <dullstart.yang@gmail.com>
2022-10-24 13:34:27 +0800

    計算BMI_表單

--- bmi.php
+++ bmi.php
@@ -10,5 +10,10 @@
     <a href="index.php">回首頁</a>
     <br>
     <h1>BMI計算</h1>
+    <form action="result.php" method="get">
+        <div>身高:<input type="munber" name="hight"></div>
+        <div>體重:<input type="munber" name="weight"></div>
+        <div><input type="submit" value="計算BMI">計算BMI</div>
+    </form>
 </body>
 </html>
