043acafc759ae9030407f0a00542d9f90396f72e
moksha <dullstart.yang@gmail.com>
2022-10-28 16:05:43 +0800

    增加了測試，並在測試中

--- calendar.php
+++ calendar.php
@@ -9,7 +9,7 @@
 
     <title>萬年曆</title>
     <style>
-        body{
+        body {
             background: url("./images/04.jpg") no-repeat;
             background-size: 100% 100%;
         }
@@ -22,15 +22,15 @@
     $cal = [];
     $year =  (isset($_GET['y'])) ? $_GET['y'] : date("Y");
     $month = (isset($_GET['m'])) ? $_GET['m'] : date("n");
-    
+//
     if ($month < 1) {
-        $month=12;
-        $year=$year-1;
-    } 
+        $month = 12;
+        $year = $year - 1;
+    }
     if ($month > 12) {
-        $month=1;
-        $year=$year+1;
-    } 
+        $month = 1;
+        $year = $year + 1;
+    }
     $prevMonth = $month - 1;
     $nextMonth = $month + 1;
 
@@ -57,37 +57,103 @@
     echo "月曆天數" . ($monthDays + $spaceDays) . "天，" . $weeks . "周";
     ?>
     <div class="tab">
-    <div class="catlog">
-        <a href="?y=<?= $year ?>&m=<?= $prevMonth ?>" class="left">&lt;</a>
-        <div><?= $year; ?>&nbsp;年&nbsp;<?= $month ?>&nbsp;月</div>
-        <a href="?y=<?= $year ?>&m=<?= $nextMonth ?>" class="right">&gt;</a>
+        <div class="catlog">
+            <a href="?y=<?= $year ?>&m=<?= $prevMonth ?>" class="left">&lt;</a>
+            <div><?= $year; ?>&nbsp;年&nbsp;<?= $month ?>&nbsp;月</div>
+            <a href="?y=<?= $year ?>&m=<?= $nextMonth ?>" class="right">&gt;</a>
+        </div>
+        <!-- <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get"> -->
+        <!-- <form action="<?php echo $_SERVER['PHP_SELF'] ?>" mothed="get"> -->
+        <label>
+            <select name='y' id=year>
+                <?php
+
+                for ($i = date("Y") - 50; $i < date("Y") + 50; $i++) {
+                    if ($i == $year) {
+                        echo "<option selected>";
+                        echo $i;
+                    } else {
+                        echo "<option >";
+                        echo $i;
+                    }
+                    echo "</option>";
+                }
+                ?>
+            </select>
+            年</label>
+        <script>
+           // var x = document.getElementById("month").selectedIndex;
+        //     document.getElementsByTagName("option")[x] = [x].value;
+            document.getElementById("year").addEventListener('change', function() {
+                 var x = document.getElementById("month").selectedIndex;
+                 var y= document.getElementsByTagName("option")[x].value;
+                 var z= y[x].value; 
+            }, false);​
+        </script>
+        <?php 
+            $year="<sprict type=text/javascript>document.write('z')</script>";
+?>
+        <?php echo $year ?>
+        <label>
+            <!-- <select id="month" onChange="location = this.options[this.selectedIndex].value;">  -->
+            <!-- <select id="month" onChange="location = this.value;"> -->
+            <!-- option:selected -->
+            <select name="m" onChange="location = this.value;">
+                <?php
+                for ($i = 1; $i <= 12; $i++) {
+                    if ($i == $month) {
+                        echo "<option selected>";
+                        echo $i;
+                    } else {
+
+                        echo "<option value=\"calendar.php?y=$year&m=$i\">";
+                        echo $i;
+                    }
+                    echo "</option>";
+                }
+                ?>
+            </select>
+            月
+        </label>
+        <!-- <input type="submit" value="查詢"> -->
+        <!-- </form> -->
+        <!-- </form> -->
+        <table>
+            <?php
+            $week = ['日', '一', '二', '三', '四', '五', '六'];
+            echo "<tr>";
+            for ($i = 0; $i < count($week); $i++) {
+                echo "<td id='firstr'>" . $week[$i] . "</td>";
+            }
+            echo "</tr>";
+            foreach ($cal as $i => $day) {
+                if ($i % 7 == 0) {
+                    echo "<tr>";
+                }
+                if ($cal[$i] == date("j")) {
+                    echo "<td id='today'>";
+                    echo $day;
+                    echo "</td>";
+                } else {
+                    echo "<td>$day</td>";
+                }
+                if ($i % 7 == 6) {
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </table>
     </div>
-    <table>
-        <?php
-        $week = ['日', '一', '二', '三', '四', '五', '六'];
-        echo "<tr>";
-        for ($i = 0; $i < count($week); $i++) {
-            echo "<td id='firstr'>" . $week[$i] . "</td>";
-        }
-        echo "</tr>";
-        foreach ($cal as $i => $day) {
-            if ($i % 7 == 0) {
-                echo "<tr>";
-            }
-            if($cal[$i]==date("j")){
-                echo "<td id='today'>";
-                echo $day;
-                echo "</td>";
-            }else{
-                echo "<td>$day</td>";
-            }
-            if ($i % 7 == 6) {
-                echo "</tr>";
-            }
-        }
-        ?>
-    </table>
-    </div>
+    <!-- <select id="month" onChange="location = this.options[this.selectedIndex].value;"> -->
+    <script>
+        // document.getElementById("month").addEventListener('change', function() {
+        //     window.location = this.value;
+        // }, true);​
+        // function displayResult() {
+        //     var x = document.getElementById("month").selectedIndex;
+        //     document.getElementsByTagName("option")[x] = [x].value;
+        // }
+    </script>
 </body>
 
 </html>
