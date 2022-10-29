ad207d10ece49f0a056c93b837fcf83649befc87
unknown <dullstart.yang.com>
2022-10-30 02:58:33 +0800

    嘗試廣告輪撥

--- calendar.php
+++ calendar.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="css/year.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>萬年曆 PH班第二期</title>
     <style>
         html {
@@ -16,60 +15,35 @@
         }
 
         .container {
-            width: 62.5%;
+            width: 52%;
             margin: 0 auto;
             border: 1rem solid #fff;
-            box-shadow: 0.1rem 0.1rem 0.2rem 0.1rem #333;
-            min-width: 40rem;
-            position: relative;
-
+            box-shadow: 0rem 0rem 0.2rem 0.1rem #333;
+            min-width: 60rem;
         }
 
         .slider {
             /*設定圖片比列(2:1)*/
             width: 100%;
             height: 0;
-            padding-bottom: 50%;
+            padding-bottom: 47%;
             position: relative;
-            /* overflow: hidden; */
-            border: 1rem solid #333;
-            /* z-index: 999; */
+            overflow: hidden;
+            border: 0.2rem solid #333;
         }
 
         #img {
-            width: 500%;
-            /*4張的寬度*/
-            position: relative;
+            width: 1300%;
+            /*13張的寬度*/
+            position: absolute;
             display: grid;
-            grid-template-columns: repeat(5, 1fr);
+            grid-template-columns: repeat(13, 1fr);
             /*新技術喔，重要*/
-            z-index: 999;
+
         }
 
         #img img {
             width: 100%;
-        }
-
-        .tab {
-            top: 10%;
-            left: 20%;
-        }
-
-        .page1 {
-            background: url("./images/1.jpg");
-
-        }
-
-        .page2 {
-            background: url("./images/2.jpg");
-        }
-
-        .page3 {
-            background: url("./images/3.jpg");
-        }
-
-        .page4 {
-            background: url("./images/4.jpg");
         }
     </style>
 </head>
@@ -81,112 +55,107 @@
         </marquee>
     </header>
 
-    <div class="container">
-        <?php
-        date_default_timezone_set("Asia/Taipei");
-        $cal = [];
-        $year =  (isset($_GET['y'])) ? $_GET['y'] : date("Y");
-        $month = (isset($_GET['m'])) ? $_GET['m'] : date("n");
-        if ($month < 1) {
-            $month = 12;
-            $year = $year - 1;
-        }
-        if ($month > 12) {
-            $month = 1;
-            $year = $year + 1;
-        }
-        $prevMonth = $month - 1;
-        $nextMonth = $month + 1;
-        $prevYear = $year - 1;
-        $nextYear = $year + 1;
+    <?php
+    date_default_timezone_set("Asia/Taipei");
+    $cal = [];
+    $year =  (isset($_GET['y'])) ? $_GET['y'] : date("Y");
+    $month = (isset($_GET['m'])) ? $_GET['m'] : date("n");
+    if ($month < 1) {
+        $month = 12;
+        $year = $year - 1;
+    }
+    if ($month > 12) {
+        $month = 1;
+        $year = $year + 1;
+    }
+    $prevMonth = $month - 1;
+    $nextMonth = $month + 1;
+    $prevYear = $year - 1;
+    $nextYear = $year + 1;
 
-        $firstDay = $year . "-" . $month . "-1"; //第一天
-        $firstDayWeek = date("w", strtotime($firstDay)); //N:1-7第一周的星期幾
-        $monthDays = date("t", strtotime($firstDay)); //當月的天數
-        $lastDay = $year . '-' . $month . '-' . $monthDays; //最後一天
-        $spaceDays = $firstDayWeek; //空格的天數
-        $weeks = ceil(($monthDays + $spaceDays) / 7); //當月的周數
-        //--------------------填入陣列--------------------//
-        for ($i = 0; $i < $spaceDays; $i++) {
-            $cal[] = '';
-        }
-        for ($i = 0; $i < $monthDays; $i++) {
-            $cal[] = date("j", strtotime("+$i days", strtotime($firstDay)));
-        }
-        // echo "<pre>";
-        // print_r($cal);
-        // echo "</pre>";
-        // echo "第一天 : " . $firstDay . "星期" . $firstDayWeek;
-        // echo "<br>";
-        // echo "該月共" . $monthDays . "天，最後一天是" . $lastDay;
-        // echo "<br>";
-        // echo "月曆天數" . ($monthDays + $spaceDays) . "天，" . $weeks . "周";
-        ?>
+    $firstDay = $year . "-" . $month . "-1"; //第一天
+    $firstDayWeek = date("w", strtotime($firstDay)); //N:1-7第一周的星期幾
+    $monthDays = date("t", strtotime($firstDay)); //當月的天數
+    $lastDay = $year . '-' . $month . '-' . $monthDays; //最後一天
+    $spaceDays = $firstDayWeek; //空格的天數
+    $weeks = ceil(($monthDays + $spaceDays) / 7); //當月的周數
+    //--------------------填入陣列--------------------//
+    for ($i = 0; $i < $spaceDays; $i++) {
+        $cal[] = '';
+    }
+    for ($i = 0; $i < $monthDays; $i++) {
+        $cal[] = date("j", strtotime("+$i days", strtotime($firstDay)));
+    }
+    // echo "<pre>";
+    // print_r($cal);
+    // echo "</pre>";
+    // echo "第一天 : " . $firstDay . "星期" . $firstDayWeek;
+    // echo "<br>";
+    // echo "該月共" . $monthDays . "天，最後一天是" . $lastDay;
+    // echo "<br>";
+    // echo "月曆天數" . ($monthDays + $spaceDays) . "天，" . $weeks . "周";
+    ?>
+    <div class="container">
         <div class="slider">
             <div id="img">
-                <!-- <?Php
-                        // for($i=1;$i<=4;$i++){
-                        //     echo "<img class='";
-                        //     echo "page".$i;
-                        //     echo "'>";
-                        // }
-                        ?> -->
-                <!-- <img  class="<?= 'page' . $month ?>"> -->
-                <img src="./images/page1.jpg" alt="">
-                <img src="./images/page2.jpg" alt="">
-                <img src="./images/page3.jpg" alt="">
-                <img src="./images/page4.jpg" alt="">
-                <img src="./images/page1.jpg" alt="">
-
+                <?Php
+                // if ($month > 12) {
+                    // $month = 1;
+                    for ($i = 0; $i < 2; $i++) {
+                        
+                        echo "<img src='images/";
+                        echo $month + ($i);
+                        echo ".jpg '>";
+                    }
+                // }
+                ?>
+                <img src="./images/1.jpg" alt="calendar">
             </div>
         </div>
-        <?php echo "page" . $month ?>
-        <div class="tab">
+        <div class="tabMiddle">
             <div class="catlog">
                 <div><a href="?y=<?= $prevYear ?>&m=<?= $month ?>" class="leftyear"><?= $year - 1 ?><i class="fa-solid fa-backward arrow"></i></a></div>
                 <div class="left previmg">&lt;</div>
-                <div style="font-family:'標楷體';font-size:3.2rem;">萬</div>
+
                 <div class="right nextimg">&gt;</div>
                 <div><a href="?y=<?= $nextYear ?>&m=<?= $month ?>" class="rightyear"><i class="fa-solid fa-forward arrow"></i><?= $year + 1 ?></a></div>
-            </div>
-
-
-            <div class="select">
-                <label>
-                    <select name='y' onChange="location = this.value;">
-                        <?php
-                        for ($i = date("Y") - 50; $i < date("Y") + 50; $i++) {
-                            if ($i == $year) {
-                                echo "<option selected>";
-                                echo $i;
-                            } else {
-                                echo "<option value=\"calendar.php?y=$i&m=$month\">";
-                                echo $i;
+                <div class="select">
+                    <label>
+                        <select name='y' onChange="location = this.value;">
+                            <?php
+                            for ($i = date("Y") - 50; $i < date("Y") + 50; $i++) {
+                                if ($i == $year) {
+                                    echo "<option selected>";
+                                    echo $i;
+                                } else {
+                                    echo "<option value=\"calendar.php?y=$i&m=$month\">";
+                                    echo $i;
+                                }
+                                echo "</option>";
                             }
-                            echo "</option>";
-                        }
-                        ?>
-                    </select>
-                    <span>年</span>
-                </label>
+                            ?>
+                        </select>
+                        <span>年</span>
+                    </label>
 
-                <label>
-                    <select name="m" onChange="location = this.value;">
-                        <?php
-                        for ($i = 1; $i <= 12; $i++) {
-                            if ($i == $month) {
-                                echo "<option selected value=\"calendar.php?y=$year&m=$i\">";
-                                echo $i;
-                            } else {
-                                echo "<option value=\"calendar.php?y=$year&m=$i\">";
-                                echo $i;
+                    <label>
+                        <select name="m" onChange="location = this.value;">
+                            <?php
+                            for ($i = 1; $i <= 12; $i++) {
+                                if ($i == $month) {
+                                    echo "<option selected value=\"calendar.php?y=$year&m=$i\">";
+                                    echo $i;
+                                } else {
+                                    echo "<option value=\"calendar.php?y=$year&m=$i\">";
+                                    echo $i;
+                                }
+                                echo "</option>";
                             }
-                            echo "</option>";
-                        }
-                        ?>
-                    </select>
-                    <span>月</span>
-                </label>
+                            ?>
+                        </select>
+                        <span>月</span>
+                    </label>
+                </div>
             </div>
         </div>
         <div class="tab">
@@ -225,7 +194,7 @@
 </body>
 <script>
     var x = 0; //手動用作標
-    const k = 4; //5張圖片
+    const k = 12; //5張圖片
     var interval; //間隔時間，自動用
     var banner = $('#img'); //簡化;
     var next = false,
@@ -248,7 +217,7 @@
             }, 800);
             setTimeout(function() {
                 next = false;
-                location.href="?y=<?= $year ?>&m=<?= $nextMonth ?>";
+                location.href = "?y=<?= $year ?>&m=<?= $nextMonth ?>";
             }, 801);
 
         };
@@ -271,7 +240,7 @@
             }, 800);
             setTimeout(function() {
                 prev = false;
-                location.href="?y=<?= $year ?>&m=<?= $prevMonth ?>";
+                location.href = "?y=<?= $year ?>&m=<?= $prevMonth ?>";
             }, 801);
         };
     });
